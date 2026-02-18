14e12f358a90dbefdc76f4c43a0be7c45eec13a9
Servet Erdoğan <eservet224@gmail.com>
2026-02-18 12:56:23 +0300

    new colors and fonts have been added

---
+++ app/src/main/java/com/serveterdogan/genetikalgoritmamobil/ui/theme/Color.kt
@@ -0,0 +1,56 @@
+package com.serveterdogan.genetikalgoritmamobil.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val Primary = Color(0xFF5140B5)
+val PrimaryLight = Color(0xFF7B6DD4)
+val PrimaryDark = Color(0xFF3A2E8A)
+
+// Background Colors
+val BackgroundLight = Color(0xFFF6F6F8)
+val BackgroundDark = Color(0xFF16141E)
+
+// Card Colors
+val CardLight = Color(0xFFFFFFFF)
+val CardDark = Color(0xFF192633)
+
+// Text Colors
+val TextPrimary = Color(0xFF111418)
+val TextSecondary = Color(0xFF92ADC9)
+val TextOnPrimary = Color(0xFFFFFFFF)
+
+// Border Colors
+val BorderLight = Color(0xFFE5E7EB)  // gray-200
+val BorderDark = Color(0xFF324D67)
+
+// Status Colors
+val Success = Color(0xFF10B981)  // green-500
+val Error = Color(0xFFEF4444)    // red-500
+val Warning = Color(0xFFF59E0B)  // amber-500
+val Info = Color(0xFF3B82F6)     // blue-500
+
+// Module Colors (for icons/cards)
+val ModuleBlue = Color(0xFF3B82F6)      // blue-500
+val ModulePurple = Color(0xFF9333EA)    // purple-600
+val ModuleOrange = Color(0xFFEA580C)    // orange-600
+val ModuleGreen = Color(0xFF16A34A)     // green-600
+val ModuleRed = Color(0xFFDC2626)       // red-600
+
+// Module Background Colors (Light)
+val ModuleBlueBgLight = Color(0xFFDCEEFF)     // blue-100
+val ModulePurpleBgLight = Color(0xFFF3E8FF)   // purple-100
+val ModuleOrangeBgLight = Color(0xFFFFEDD5)   // orange-100
+val ModuleGreenBgLight = Color(0xFFDCFCE7)    // green-100
+val ModuleRedBgLight = Color(0xFFFEE2E2)      // red-100
+
+// Module Background Colors (Dark)
+val ModuleBlueBgDark = Color(0xFF1E3A8A)      // blue-900/30
+val ModulePurpleBgDark = Color(0xFF581C87)    // purple-900/30
+val ModuleOrangeBgDark = Color(0xFF7C2D12)    // orange-900/30
+val ModuleGreenBgDark = Color(0xFF14532D)     // green-900/30
+val ModuleRedBgDark = Color(0xFF7F1D1D)       // red-900/30
+
+// Surface Colors
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF192633)
